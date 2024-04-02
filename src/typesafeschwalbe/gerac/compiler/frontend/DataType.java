55b1da62b38d580efe1fe284e6ee1798d2bb1301
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-04-02 17:05:58 +0300

    Add methods to 'DataType'

--- src/typesafeschwalbe/gerac/compiler/frontend/DataType.java
+++ src/typesafeschwalbe/gerac/compiler/frontend/DataType.java
@@ -1,6 +1,7 @@
 
 package typesafeschwalbe.gerac.compiler.frontend;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +13,7 @@ public final class DataType {
         DataType elementType
     ) {}
 
-    public static record Object(
+    public static record UnorderedObject(
         Map<String, DataType> memberTypes,
         boolean expandable
     ) {}
@@ -33,17 +34,17 @@ public final class DataType {
     ) {}
 
     public enum Type {
-        UNKNOWN,         // = null
-        UNIT,            // = null
-        BOOLEAN,         // = null
-        INTEGER,         // = null
-        FLOAT,           // = null
-        STRING,          // = null
-        ARRAY,           // Array
-        OBJECT,          // Object
-        CONCRETE_OBJECT, // ConcreteObject
-        CLOSURE,         // Closure
-        UNION            // Union
+        UNKNOWN,          // = null
+        UNIT,             // = null
+        BOOLEAN,          // = null
+        INTEGER,          // = null
+        FLOAT,            // = null
+        STRING,           // = null
+        ARRAY,            // Array
+        UNORDERED_OBJECT, // UnorderedObject
+        CONCRETE_OBJECT,  // ConcreteObject
+        CLOSURE,          // Closure
+        UNION             // Union
     }
 
     public final Type type;
@@ -71,24 +72,165 @@ public final class DataType {
         this.age = 0;
     }
 
+    private DataType(Type type, Object value, Source source, long age) {
+        this.type = type;
+        this.value = value;
+        this.source = source;
+        this.age = age;
+    }
+
     @SuppressWarnings("unchecked")
     public <T> T getValue() {
         return (T) this.value;
     }
 
     public boolean isExpandable() {
-        // TODO: DETERMINE IF ALL MEMBERS, VARIANTS AND ELEMENTS
-        //       AND THE TYPE ITSELF IS EXPANDABLE
-        //       (ONLY RETURN FALSE IF NOTHING CAN BE EXPANDED)
-        throw new RuntimeException("not yet implemented");
+        switch(this.type) {
+            case UNKNOWN:
+            case UNIT:
+            case BOOLEAN:
+            case INTEGER:
+            case FLOAT:
+            case STRING: {
+                return false;
+            }
+            case ARRAY: {
+                return this.<Array>getValue().elementType.isExpandable();
+            }
+            case UNORDERED_OBJECT: {
+                UnorderedObject data = this.getValue();
+                for(DataType memberType: data.memberTypes.values()) {
+                    if(memberType.isExpandable()) {
+                        return true;
+                    }
+                }
+                return data.expandable;
+            }
+            case CONCRETE_OBJECT: {
+                ConcreteObject data = this.getValue();
+                for(DataType memberType: data.memberTypes) {
+                    if(memberType.isExpandable()) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            case CLOSURE: {
+                Closure data = this.getValue();
+                for(DataType argumentType: data.argumentTypes) {
+                    if(argumentType.isExpandable()) {
+                        return true;
+                    }
+                }
+                return data.returnType.isExpandable();
+            }
+            case UNION: {
+                Union data = this.getValue();
+                for(DataType memberType: data.variants.values()) {
+                    if(memberType.isExpandable()) {
+                        return true;
+                    }
+                }
+                return data.expandable;
+            }
+            default: {
+                throw new RuntimeException("type not handled!");
+            }
+        }
     }
 
     public static DataType unify(DataType a, DataType b) {
-        // TODO: COMBINE THE TWO TYPES AND ALL MEMBERS, VARIANTS AND ELEMENTS
-        //       COMBINE VARIANTS AND MEMBERS IF EXPANDABLE
-        //       ERROR IF THEY DON'T MATCH UP, CHOOSE THE SOURCE AND AGE
-        //       OF THE TYPE WITH THE HIGHEST AGE AND INCREMENT THE AGE
+        // TODO: - COMBINE THE TWO TYPES AND ALL MEMBERS, VARIANTS AND ELEMENTS
+        //       - COMBINE VARIANTS AND MEMBERS IF EXPANDABLE
+        //       - ERROR IF THEY DON'T MATCH UP
+        //       - CHOOSE THE SOURCE AND AGE
+        //         OF THE TYPE WITH THE HIGHEST AGE AND INCREMENT THE AGE
         throw new RuntimeException("not yet implemented");
+    }
+
+    public DataType replace(DataType replaced, DataType replacement) {
+        if(this == replaced) {
+            return replacement;
+        }
+        switch(this.type) {
+            case UNKNOWN:
+            case UNIT:
+            case BOOLEAN:
+            case INTEGER:
+            case FLOAT:
+            case STRING: {
+                return this;
+            }
+            case ARRAY: {
+                Array data = this.getValue();
+                DataType elementType = data.elementType()
+                    .replace(replaced, replacement);
+                return new DataType(
+                    this.type, new Array(elementType), this.source, this.age
+                );
+            }
+            case UNORDERED_OBJECT: {
+                UnorderedObject data = this.getValue();
+                Map<String, DataType> memberTypes = new HashMap<>();
+                for(String memberName: data.memberTypes().keySet()) {
+                    DataType memberType = data.memberTypes().get(memberName)
+                        .replace(replaced, replacement);
+                    memberTypes.put(memberName, memberType);
+                }
+                return new DataType(
+                    this.type, 
+                    new UnorderedObject(memberTypes, data.expandable()),
+                    this.source,
+                    this.age
+                );
+            }
+            case CONCRETE_OBJECT: {
+                ConcreteObject data = this.getValue();
+                List<DataType> memberTypes = data.memberTypes()
+                    .stream().map(memberType -> memberType.replace(
+                        replaced, replacement
+                    )).toList();
+                return new DataType(
+                    this.type,
+                    new ConcreteObject(data.memberNames(), memberTypes),
+                    this.source,
+                    this.age
+                );
+            }
+            case CLOSURE: {
+                Closure data = this.getValue();
+                List<DataType> argumentTypes = data.argumentTypes()
+                    .stream().map(argumentType -> argumentType.replace(
+                        replaced, replacement
+                    )).toList();
+                DataType returnType = data.returnType()
+                    .replace(replaced, replacement);
+                return new DataType(
+                    this.type,
+                    new Closure(argumentTypes, returnType),
+                    this.source,
+                    this.age
+                );
+            }
+            case UNION: {
+                Union data = this.getValue();
+                Map<String, DataType> variants = new HashMap<>();
+                for(String memberName: data.variants().keySet()) {
+                    DataType memberType = data.variants().get(memberName)
+                        .replace(replaced, replacement);
+                    variants.put(memberName, memberType);
+                }
+                return new DataType(
+                    this.type, 
+                    new Union(variants, data.expandable()),
+                    this.source,
+                    this.age
+                );
+            }
+            default: {
+                throw new RuntimeException("type not handled!");
+            }
+        }
     }
 
 }
